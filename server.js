e8563ddcb7ab3d96e2a731e6e45e925d91e5ed41
Annapoorna G <annapoornasunitha@gmail.com>
2016-09-25 21:01:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.gete('/ui/Kung-Fu-Panda-2-icon.png', function(req,res){
+    res.sendFile(path.join(_dirname,'ui','Kung-Fu-Panda-2-icon.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
