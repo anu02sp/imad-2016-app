5104339e084692f3f49cb62bf27d0f0caeb5a21e
Annapoorna G <annapoornasunitha@gmail.com>
2016-09-28 14:39:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
 
-var img=document.getElementBYId('madi');
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft= '100 px';
+}
