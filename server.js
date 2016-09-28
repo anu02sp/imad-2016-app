2e427f44f0aeebc70dc117386dbaa6cc5ffcc88b
Annapoorna G <annapoornasunitha@gmail.com>
2016-09-28 12:24:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', function (req, res) {
 
 app.get('/articleName',function ( req,res) {
     var articleName =req.params.articleName;
-    res.sendFile(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
